e0eecd0d14cc68e7dbedc71df466f47d09123fdc
franktominc <frankptominc@gmail.com>
2015-02-25 13:52:15 -0300

    Fixed entities

--- app/models/CarAccident.java
+++ app/models/CarAccident.java
@@ -26,4 +26,69 @@ public class CarAccident {
     private List<Victim> victims;
     @OneToMany(cascade = CascadeType.ALL)
     private List<Vehicle> vehicles;
+    private String cause;
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCrossroad() {
+        return crossroad;
+    }
+
+    public void setCrossroad(String crossroad) {
+        this.crossroad = crossroad;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<Victim> getVictims() {
+        return victims;
+    }
+
+    public void setVictims(List<Victim> victims) {
+        this.victims = victims;
+    }
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(List<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
 }
--- app/models/Vehicle.java
+++ app/models/Vehicle.java
@@ -13,4 +13,20 @@ public class Vehicle extends Model{
     @Id
     private Long id;
     private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
