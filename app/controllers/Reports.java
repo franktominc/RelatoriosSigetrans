6066608420d3cd98f124241acd5bc59289d98959
franktominc <frankptominc@gmail.com>
2015-03-24 19:35:11 -0300

    Added full bootstrap support

--- app/controllers/Reports.java
+++ app/controllers/Reports.java
@@ -18,7 +18,12 @@ import java.util.*;
  * Created by ftominc on 19/03/15.
  */
 public class Reports extends Controller{
-
+    /**
+     * Generate a report of the neighborhoods
+     * with most Car Accidents
+     * @return Action Page with neighborhoods sorted
+     * by Car Accidents
+     */
     public static Result neighborhoodReport(){
 
         String sql = "select neighborhood,count(Id) as c " +
@@ -29,27 +34,9 @@ public class Reports extends Controller{
 
         SqlQuery sqlQuery = Ebean.createSqlQuery(sql);
         List<SqlRow> list = sqlQuery.findList().subList(0,10);
-        List<ReportData> l = new ArrayList<ReportData>(){
-            @Override
-            public String toString(){
-                String result = "[['Bairro', 'Quantidade'],";
-                for (int i = 0; i < this.size(); i++){
-                    result += this.get(i) + " ";
-                    if(i< this.size() -1){
-                        result += ",";
-                    }
-                }
-                result += "]";
-                return result;
-            }
-        };
-
-        for(SqlRow s: list){
-            l.add(new ReportData(s.getString("neighborhood"), s.getInteger("c")));
-        }
+        List l = find(sql);
         return ok(test.render(l));
     }
-    @Transactional
     public static Result neighborhoodReportByDate(){
         DateFilter dateFilter = Form.form(DateFilter.class).bindFromRequest().get();
 
